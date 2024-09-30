68792815568298dd36ff163d67846f156b1567be
YahyaKhaliman <yahyaindrayana35@gmail.com>
2024-09-30 13:23:22 +0700

    itemControllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -9,7 +9,6 @@ const addItem = async (req, res) => {
   try {
     console.log("Received data:", req.body);
 
-    // Menambahkan item ke dalam database
     const item = await itemModel.addItem(
       req.body.name,
       req.body.type,
@@ -19,13 +18,11 @@ const addItem = async (req, res) => {
     const qrData = `https://qrstok-api.my.id/items/s/${item.secretCode}`;
     const dir = path.join(__dirname, "../public/qrcodes");
 
-    // Memeriksa apakah direktori untuk menyimpan QR Code ada, jika tidak buat
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
 
-    // Membuat QR Code
-    QRCode.toDataURL(qrData, (err, url) => {
+    QRCode.toDataURL(qrData, async (err, url) => {
       if (err) {
         console.error("Error generating QR Code:", err);
         return res.status(500).send(err.message);
@@ -34,7 +31,6 @@ const addItem = async (req, res) => {
       const base64Data = url.replace(/^data:image\/png;base64,/, "");
       const filePath = path.join(dir, `${item.secretCode}.png`);
 
-      // Menyimpan QR Code sebagai file PNG
       fs.writeFile(filePath, base64Data, "base64", async (err) => {
         if (err) {
           console.error("Error saving QR Code:", err);
@@ -42,16 +38,19 @@ const addItem = async (req, res) => {
         }
 
         try {
-          // Setelah QR Code berhasil disimpan, update item di database untuk menyimpan path qrCode
           const qrCodePath = `/public/qrcodes/${item.secretCode}.png`;
-
-          // Update database dengan path QR Code
           await itemModel.updateQrCode(item.id, qrCodePath);
 
           res.status(201).json({
-            item,
-            message: "QR Code created, saved, and path stored in database",
-            qrCodePath,
+            status: "success",
+            message: "Item added and QR Code created successfully",
+            item: {
+              id: item.id,
+              name: item.name,
+              type: item.type,
+              stock: item.stock,
+              qrCode: qrCodePath,
+            },
           });
         } catch (dbError) {
           console.error("Error updating item with QR Code path:", dbError);
