0b4372706af1574a69f06462e0e9d6279d421de2
YahyaKhaliman <yahyaindrayana35@gmail.com>
2024-10-01 15:20:26 +0700

    itemsRoutes

--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -9,13 +9,12 @@ router.post("/items", itemsController.addItem);
 // Endpoint untuk mendapatkan semua item
 router.get("/items", itemsController.getAllItems);
 
-// Endpoint untuk mendapatkan total stock
-router.get("/items/total-stock", itemsController.getTotalStock);
-
 // Endpoint untuk mendapatkan item berdasarkan secretCode
 router.get("/items/s/:secretCode", itemsController.getItemBySecretCode);
 
-// Uncomment these lines if you want to include update and delete functionality
+// Endpoint untuk mendapatkan total stock
+router.get("/items/total-stock", itemsController.getTotalStock);
+
 // router.put("/items/:id", itemsController.updateItem);
 // router.delete("/items/:id", itemsController.deleteItem);
 
