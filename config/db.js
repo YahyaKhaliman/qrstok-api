05267c5b406a5582eae86dfcf064cc7837421719
YahyaKhaliman <yahyaindrayana35@gmail.com>
2024-10-25 09:18:24 +0700

    database

--- config/db.js
+++ config/db.js
@@ -6,19 +6,25 @@ const mysql = require("mysql2");
 //   password: process.env.local_password,
 //   database: process.env.local_database,
 // });
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.host,
   user: process.env.user,
   password: process.env.password,
   database: process.env.database,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  connectionLimit: 10, // Jumlah maksimal koneksi dalam pool
+  queueLimit: 0, // Tidak ada batasan antrian
+  connectTimeout: 10000, // Timeout koneksi (opsional)
+  acquireTimeout: 10000, // Timeout untuk mengambil koneksi dari pool (opsional)
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL database!");
+connection.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+    throw err;
+  }
+  if (connection) connection.release(); // Koneksi yang berhasil dilepaskan kembali ke pool
+  console.log("Connected to MySQL database");
 });
 
 module.exports = connection;
