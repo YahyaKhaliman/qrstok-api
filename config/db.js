c29d7de8d82c52a09f2b53942c7347b9c365e31f
Yahyakhaliman <yahyaindrayana35@gmail.com>
2026-02-20 08:33:59 +0700

    Refactor code structure for improved readability and maintainability

--- config/db.js
+++ config/db.js
@@ -1,30 +1,22 @@
 const mysql = require("mysql2");
 
-// const connection = mysql.createConnection({
-//   host: process.env.local_host,
-//   user: process.env.local_user,
-//   password: process.env.local_password,
-//   database: process.env.local_database,
-// });
-const connection = mysql.createPool({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
+const pool = mysql.createPool({
+  host: process.env.local_host,
+  user: process.env.local_user,
+  password: process.env.local_password,
+  database: process.env.local_database,
   waitForConnections: true,
-  connectionLimit: 10, // Jumlah maksimal koneksi dalam pool
-  queueLimit: 0, // Tidak ada batasan antrian
-  connectTimeout: 10000, // Timeout koneksi (opsional)
-  acquireTimeout: 10000, // Timeout untuk mengambil koneksi dari pool (opsional)
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.getConnection((err, connection) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to MySQL database:", err);
     throw err;
   }
-  if (connection) connection.release(); // Koneksi yang berhasil dilepaskan kembali ke pool
+  if (connection) connection.release();
   console.log("Connected to MySQL database");
 });
 
-module.exports = connection;
+module.exports = pool;
